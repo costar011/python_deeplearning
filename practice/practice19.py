8877138358dd7fcb0cd5eb323b8bc8193a9e7091
costar011 <dnaer456@gmail.com>
2024-09-16 23:50:05 +0900

    quiz 5

--- practice/practice19.py
+++ practice/practice19.py
@@ -2,8 +2,18 @@
 
 # 출석번호가 1, 2, 3, 4 앞에 100을 붙이기로 함 -> 101, 102, 103, 104
 
-students = [1, 2, 3, 4, 5]
+# students = [1, 2, 3, 4, 5]
+# print(students)
+
+# students = [i+100 for i in students] # i에다가 100을 더한 값을 리스트에 넣겠다
+# print(students)
+
+# 학생 이름을 길이로 변환
+students = ["Iron man","thor","groot"]
+students = [len(i) for i in students] # len 함수는 문자열의 길이를 반환
 print(students)
 
-students = [i+100 for i in students] # i에다가 100을 더한 값을 리스트에 넣겠다
+# 학생 이름을 대문자로 변환
+students = ["Iron man","thor","groot"]
+students = [i.upper() for i in students] # upper 함수는 대문자로 변환
 print(students)

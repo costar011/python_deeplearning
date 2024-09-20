7474db37f18d7a9963eba0038fdc647a0626a849
costar011 <dnaer456@gmail.com>
2024-09-20 22:42:48 +0900

    quiz6

--- practice/practice24.py
+++ practice/practice24.py
@@ -16,5 +16,6 @@ def checkpoint_ret(gun, soldiers):
     return gun
 
 print("전체 총: {0}".format(gun))
-checkpoint(2) # 2명이 경계근무 나감
+# checkpoint(2) # 2명이 경계근무 나감
+gun = checkpoint_ret(gun, 2) # 2명이 경계근무 나감
 print("남은 총: {0}".format(gun)) # 전체 총 출력

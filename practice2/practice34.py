5f983d647f767cc0087b2b9e0e5ac67bf886160f
costar011 <dnaer456@gmail.com>
2024-09-29 23:08:02 +0900

    nado coding

--- practice2/practice34.py
+++ practice2/practice34.py
@@ -1,15 +1,20 @@
-# 다중 상속
+# 메소드 오버라이딩
 
 # 일반 유닛
 class Unit:
     def __init__(self, name, hp, damage):  # __init__ : 파이썬에서 쓰이는 생성자
         self.name = name  # 멤버 변수
         self.hp = hp
+        self.speed = speed
+
+    def move(self, location):
+        print("[지상 유닛 이동]")
+        print("{0} : {1} 방향으로 이동합니다. [속도 {2}]".format(self.name, location, self.speed))
 
 # 공격 유닛
 class AttackUnit(Unit):  # 상속
-    def __init__(self, name, hp, damage):  # __init__ : 파이썬에서 쓰이는 생성자
-        Unit.__init__(self, name, hp, damage)
+    def __init__(self, name, hp, speed, damage):  # __init__ : 파이썬에서 쓰이는 생성자
+        Unit.__init__(self, name, hp, damage, speed)
         self.damage = damage
 
     def attack(self, location):
@@ -32,10 +37,15 @@ class Flyable:
 
 # 공중 공격 유닛 클래스
 class FlyableAttackUnit(AttackUnit, Flyable):  # 다중 상속
-    def __init__(self, name, hp, damage, flying_speed):  # __init__ : 파이썬에서 쓰이는 생성자
-        AttackUnit.__init__(self, name, hp, damage)
+    def __init__(self, name, hp, damage, flying_speed):  
+        AttackUnit.__init__(self, name, hp, 0, damage) # 지상 speed 0
         Flyable.__init__(self, flying_speed)
 
-# 발키리 : 공중 공격 유닛, 한 번에 14발 미사일 발사
-valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
-valkyrie.fly(valkyrie.name, "3시") # Flyable 클래스의 fly 메소드 호출
+# 벌쳐 : 지상 유닛, 기동성이 좋음
+vulture = AttackUnit("벌쳐", 80, 10, 20)
+
+# 배틀크루저 : 공중 유닛, 체력도 높음, 공격력도 강함
+battlecruiser = FlyableAttackUnit("배틀크루저", 500, 25, 3)
+
+vulture.move("11시")
+battlecruiser.fly(battlecruiser.name, "9시")  # Flyable 클래스의 fly 메소드 사용

4180eeb1b2a7dbba51d9ad64657910f9c96c3130
costar011 <dnaer456@gmail.com>
2024-10-06 22:43:29 +0900

    house

--- Quiz/quiz8.py
+++ Quiz/quiz8.py
@@ -20,3 +20,17 @@ class House:
     # 매물 정보 표시
     def show_detail(self):
         print(self.location, self.house_type, self.deal_type, self.price, self.completion_year)
+
+house = []
+house1 = House("강남", "아파트", "매매", "10억", "2010년")
+house2 = House("마포", "오피스텔", "전세", "5억", "2007년")
+house3 = House("송파", "빌라", "월세", "500/50", "2000년")
+
+house.append(house1)
+house.append(house2)
+house.append(house3)
+
+print("총 {0}대의 매물이 있습니다.".format(len(house)))
+
+for i in house:
+    i.show_detail()

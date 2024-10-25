f3e1d621a53f2fcde719c3c0c9f1e96f76a525a3
costar011 <dnaer456@gmail.com>
2024-10-25 21:51:53 +0900

    module

--- practice2/practice39.py
+++ practice2/practice39.py
@@ -10,4 +10,7 @@ def price_morning(people):
 
 # 군인 할인 가격
 def price_soldier(people):
-    print("{0}명 군인 할인 가격은 {1}원 입니다.".format(people, people * 4000))
+    print("{0}명 군인 할인 가격은 {1}원 입니다.".format(people, people * 4000)) 
+
+
+# practice40 에서 실행

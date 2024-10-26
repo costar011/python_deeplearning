f3c28d972284756e15e5999ba2909b330819406a
costar011 <dnaer456@gmail.com>
2024-10-26 22:02:37 +0900

    theater install err

--- practice2/practice40.py
+++ practice2/practice40.py
@@ -1,16 +1,20 @@
 import theater_module
 
-# theater_module.price(3) # 3명이서 영화 보러 갔을 때 가격
-# theater_module.price_morning(4) # 4명이서 조조 할인 영화 보러 갔을 때 가격
-# theater_module.price_soldier(5) # 5명이서 군인 할인 영화 보러 갔을 때 가격
+theater_module.price(3) # 3명이서 영화 보러 갔을 때 가격
+theater_module.price_morning(4) # 4명이서 조조 할인 영화 보러 갔을 때 가격
+theater_module.price_soldier(5) # 5명이서 군인 할인 영화 보러 갔을 때 가격
 
 # import theater_module as mv
 # mv.price(3)
 # mv.price_morning(4)
 # mv.price_soldier(5)
 
-from theater_module import *
+# from theater_module import *
 # from random import *
-price(3)
-price_morning(4)
-price_soldier(5)
+# # price(3)
+# # price_morning(4)
+# # price_soldier(5)
+
+# from theater_module import price_soldier as price
+# price(5) # 5명 군인 할인 가격으로 영화 보러 갔을 때 가격
+

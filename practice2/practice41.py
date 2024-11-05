2fc147d2f4b63f6e6468225aaadce83a8585391b
costar011 <dnaer456@gmail.com>
2024-11-05 23:51:11 +0900

    trip

--- practice2/practice41.py
+++ practice2/practice41.py
@@ -1,5 +1,12 @@
 # 패키지
 
-import travel.thailand
-trip_to = travel.thailand.ThailandPackage()
+# import travel.thailand
+# import travel.thailand.ThailandPackage
+# trip_to = travel.thailand.ThailandPackage()
+# trip_to.detail()
+
+from travel.thailand import ThailandPackage
+trip_to = ThailandPackage()
 trip_to.detail()
+
+from travel import vietnam

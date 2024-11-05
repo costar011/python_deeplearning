95c6b1126a2859d0289b3c374cf21e87740a8d6f
costar011 <dnaer456@gmail.com>
2024-11-06 00:01:46 +0900

    trip

--- practice2/practice41.py
+++ practice2/practice41.py
@@ -5,10 +5,11 @@
 # trip_to = travel.thailand.ThailandPackage()
 # trip_to.detail()
 
-from travel.thailand import ThailandPackage
-trip_to = ThailandPackage()
-trip_to.detail()
+# from travel import vietnam
+# trip_to = vietnam.VietnamPackage()
+# trip_to.detail()
 
-from travel import vietnam
+# from travel import *
+from travel import *
 trip_to = vietnam.VietnamPackage()
 trip_to.detail()

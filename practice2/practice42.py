c6763ae7968157b3c43ee4eb234d3eef9d912208
costar011 <dnaer456@gmail.com>
2024-11-07 06:43:22 +0900

    init

---
+++ practice2/practice42.py
@@ -0,0 +1,16 @@
+# __all__
+
+# import travel.thailand
+# import travel.thailand.ThailandPackage
+# trip_to = travel.thailand.ThailandPackage()
+# trip_to.detail()
+
+# from travel import vietnam
+# trip_to = vietnam.VietnamPackage()
+# trip_to.detail()
+
+# from travel import *
+from travel import *
+# trip_to = vietnam.VietnamPackage()
+trip_to = travel.thailand.ThailandPackage()
+trip_to.detail() 
